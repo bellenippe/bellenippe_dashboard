7828570c42a132234befff12a1b4567bbbc2eb8a
Jessy <you@example.com>
2024-05-20 17:28:39 +0200

    fixd

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,7 +3,7 @@ import Order from "@/lib/models/Order";
 import Product from "@/lib/models/Product";
 import { connectToDB } from "@/lib/mongoDB";
 import { NextRequest, NextResponse } from "next/server";
-import { stripe } from "@/lib/stripe";
+import { stripe } from "../../../lib/stripe";
 
 export const POST = async (req: NextRequest) => {
   try {
