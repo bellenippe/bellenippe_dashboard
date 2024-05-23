298783a6a243a34dffee112c34526e66832026e5
Jessy <you@example.com>
2024-05-23 11:48:18 +0200

    remove param mails

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,19 +1,19 @@
 // import ConfirmOrder from "@/app/emails/ConfirmOrder";
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 // import { Resend } from "resend";
 
-// export async function POST() {
-//   const resend = new Resend(process.env.RESEND_API_KEY);
-//   try {
-//     const { data } = await resend.emails.send({
-//       from: "onboarding@resend.dev",
-//       to: "inthegleam01@gmail.com",
-//       subject: "Confirmation de commande",
-//       react: ConfirmOrder(),
-//     });
-//     return NextResponse.json(data, { status: 200 });
-//   } catch (error) {
-//     console.error("[orderId_GET]", error);
-//     return new NextResponse("Internal Server Error", { status: 500 });
-//   }
-// }
+export async function POST() {
+  // const resend = new Resend(process.env.RESEND_API_KEY);
+  // try {
+  //   const { data } = await resend.emails.send({
+  //     from: "onboarding@resend.dev",
+  //     to: "inthegleam01@gmail.com",
+  //     subject: "Confirmation de commande",
+  //     react: ConfirmOrder(),
+  //   });
+  //   return NextResponse.json(data, { status: 200 });
+  // } catch (error) {
+  //   console.error("[orderId_GET]", error);
+  //   return new NextResponse("Internal Server Error", { status: 500 });
+  // }
+}
